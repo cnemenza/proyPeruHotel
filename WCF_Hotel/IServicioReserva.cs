844a80394b7f37577a648b442ba1f96346343986
cneroj <christiannemenza@gmail.com>
2018-11-08 10:44:32 -0500

    Se agrego servicio reserva

--- WCF_Hotel/IServicioReserva.cs
+++ WCF_Hotel/IServicioReserva.cs
@@ -12,6 +12,317 @@ namespace WCF_Hotel
     public interface IServicioReserva
     {
         [OperationContract]
-        void DoWork();
+
+        List<dataReserva> ListarReservasEntreFechas(DateTime fecini, DateTime fecfin);
+    }
+    [DataContract]
+    [Serializable]
+    public class dataReserva
+    {
+        private String _idReserva;
+        private System.DateTime _reserFechaReser;
+        private System.DateTime _reserFechaIngreso;
+        private System.DateTime _reserFechaSalida;
+        private String _idCliente;
+        private String _nombre;
+        private Single _cantHab;
+        private Single _cantDias;
+        private Single _monHab;
+        private Single _monServicio;
+        private Single _total;
+        private String _estado;
+
+        //detalle servicio
+        private String _idServicio;
+        private String _serviNom;
+        private Single _serviPrecio;
+        private Single _cantidadServicio;
+
+        //detalle habitacion
+        private String _idHabitacion;
+        private String _tipoNombre;
+        private Single _tipoPrecio;
+        private String _habiNum;
+
+        [DataMember]
+        public string IdReserva
+        {
+            get
+            {
+                return _idReserva;
+            }
+
+            set
+            {
+                _idReserva = value;
+            }
+        }
+
+        [DataMember]
+        public string HabiNum
+        {
+            get
+            {
+                return _habiNum;
+            }
+
+            set
+            {
+                _habiNum = value;
+            }
+        }
+
+        [DataMember]
+        public DateTime ReserFechaReser
+        {
+            get
+            {
+                return _reserFechaReser;
+            }
+
+            set
+            {
+                _reserFechaReser = value;
+            }
+        }
+
+        [DataMember]
+        public DateTime ReserFechaIngreso
+        {
+            get
+            {
+                return _reserFechaIngreso;
+            }
+
+            set
+            {
+                _reserFechaIngreso = value;
+            }
+        }
+
+        [DataMember]
+        public DateTime ReserFechaSalida
+        {
+            get
+            {
+                return _reserFechaSalida;
+            }
+
+            set
+            {
+                _reserFechaSalida = value;
+            }
+        }
+
+        [DataMember]
+        public string IdCliente
+        {
+            get
+            {
+                return _idCliente;
+            }
+
+            set
+            {
+                _idCliente = value;
+            }
+        }
+
+        [DataMember]
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+
+            set
+            {
+                _nombre = value;
+            }
+        }
+
+        [DataMember]
+        public Single CantHab
+        {
+            get
+            {
+                return _cantHab;
+            }
+
+            set
+            {
+                _cantHab = value;
+            }
+        }
+
+        [DataMember]
+        public Single CantDias
+        {
+            get
+            {
+                return _cantDias;
+            }
+
+            set
+            {
+                _cantDias = value;
+            }
+        }
+
+        [DataMember]
+        public Single MonHab
+        {
+            get
+            {
+                return _monHab;
+            }
+
+            set
+            {
+                _monHab = value;
+            }
+        }
+
+        [DataMember]
+        public Single MonServicio
+        {
+            get
+            {
+                return _monServicio;
+            }
+
+            set
+            {
+                _monServicio = value;
+            }
+        }
+
+        [DataMember]
+        public Single Total
+        {
+            get
+            {
+                return _total;
+            }
+
+            set
+            {
+                _total = value;
+            }
+        }
+
+        [DataMember]
+        public string Estado
+        {
+            get
+            {
+                return _estado;
+            }
+
+            set
+            {
+                _estado = value;
+            }
+        }
+
+        [DataMember]
+        public string IdServicio
+        {
+            get
+            {
+                return _idServicio;
+            }
+
+            set
+            {
+                _idServicio = value;
+            }
+        }
+
+        [DataMember]
+        public string ServiNom
+        {
+            get
+            {
+                return _serviNom;
+            }
+
+            set
+            {
+                _serviNom = value;
+            }
+        }
+
+        [DataMember]
+        public Single ServiPrecio
+        {
+            get
+            {
+                return _serviPrecio;
+            }
+
+            set
+            {
+                _serviPrecio = value;
+            }
+        }
+
+        [DataMember]
+        public Single CantidadServicio
+        {
+            get
+            {
+                return _cantidadServicio;
+            }
+
+            set
+            {
+                _cantidadServicio = value;
+            }
+        }
+
+        [DataMember]
+        public string IdHabitacion
+        {
+            get
+            {
+                return _idHabitacion;
+            }
+
+            set
+            {
+                _idHabitacion = value;
+            }
+        }
+
+        [DataMember]
+        public string TipoNombre
+        {
+            get
+            {
+                return _tipoNombre;
+            }
+
+            set
+            {
+                _tipoNombre = value;
+            }
+        }
+
+        [DataMember]
+        public Single TipoPrecio
+        {
+            get
+            {
+                return _tipoPrecio;
+            }
+
+            set
+            {
+                _tipoPrecio = value;
+            }
+        }
     }
 }
+
--- WCF_Hotel/ServicioReserva.cs
+++ WCF_Hotel/ServicioReserva.cs
@@ -10,8 +10,61 @@ namespace WCF_Hotel
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioReserva" en el código y en el archivo de configuración a la vez.
     public class ServicioReserva : IServicioReserva
     {
-        public void DoWork()
+        public List<dataReserva> ListarReservasEntreFechas(DateTime fecini, DateTime fecfin)
         {
+            PeruHotelEntities MiHotel = new PeruHotelEntities();
+            List<dataReserva> objListaReserva = new List<dataReserva>();
+
+            try
+            {
+                //haciendo la consulta con LINQ, sin usar el SP
+                var query = (from reser in MiHotel.vw_VistaReserva
+                             where fecini < reser.reser_fechaReser &&
+                             fecfin > reser.reser_fechaReser
+                             select new //par amandar el campo proyectado
+                             {
+                                 id_reserva = reser.id_reserva,
+                                 reser_fechaReser = reser.reser_fechaReser,
+                                 reser_fechaIngreso = reser.reser_fechaIngreso,
+                                 reser_fechaSalida = reser.reser_fechaSalida,
+                                 id_cliente = reser.id_cliente,
+                                 nombre = reser.Nombre,
+                                 cant_habi = reser.Cant_Hab,
+                                 cant_dias = reser.Cant_Dias,
+                                 monto_habi = reser.Monto_hab,
+                                 monto_servicio = reser.Monto_servicio,
+                                 total = reser.Total,
+                                 estado = reser.estado
+                             });
+
+                foreach (var resultado in query)
+                {
+                    dataReserva objReservaBE = new dataReserva();
+
+                    objReservaBE.IdReserva = resultado.id_reserva;
+                    objReservaBE.ReserFechaIngreso = Convert.ToDateTime(resultado.reser_fechaIngreso);
+                    objReservaBE.ReserFechaReser = Convert.ToDateTime(resultado.reser_fechaReser);
+                    objReservaBE.ReserFechaSalida = Convert.ToDateTime(resultado.reser_fechaSalida);
+                    objReservaBE.IdCliente = resultado.id_cliente;
+                    objReservaBE.Nombre = resultado.nombre;
+                    objReservaBE.CantHab = Convert.ToSingle(resultado.cant_habi);
+                    objReservaBE.CantDias = Convert.ToSingle(resultado.cant_dias);
+                    objReservaBE.MonHab = Convert.ToSingle(resultado.monto_habi);
+                    objReservaBE.MonServicio = Convert.ToSingle(resultado.monto_servicio);
+                    objReservaBE.Total = Convert.ToSingle(resultado.total);
+                    objReservaBE.Estado = resultado.estado;
+
+
+                    //Agregamos la instancia a la alista de Data Contractual a la lista de Vendedores
+                    objListaReserva.Add(objReservaBE);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            return objListaReserva;
         }
     }
 }
